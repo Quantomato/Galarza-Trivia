8114d78bcdcd3b91e693132ba06f2fe59d927e93
FilmDragons <nagsid.puli@gmail.com>
2024-10-05 19:34:08 -0400

    Added in the listOfQuestions
    Additions to the class should follow the given framework, and if changes occur please let me know.

---
+++ Galarza Trivia/Assets/Scripts/listOfQuestions.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class listOfQuestions
+{
+    question science0 = new question("What unit is electric power measured in?", 
+        new string[] {"Watts", "Ohms", "Newtons", "Fractals"});
+    question science1 = new question("What is the name of the scientist who discovered gravity?", 
+        new string[] { "Isaac Newton", "Robert Oppenheimer", "Albert Einstein", "Benjamin Franklin" });
+    question science2 = new question("What is the name of the galaxy we live in?", 
+        new string[] {"The Milky Way", "The Pinwheel", "The Andromeda", "The Triangulum"});
+    question science3 = new question("What medicine is commonly used to treat bacteria?",
+        new string[] { "Antibiotics", "Vaccines", "Antiseptics", "Neosporin" });
+    question science4 = new question("What are the base instructions for all life?",
+        new string[] { "DNA", "RNA", "MRNA", "PNA" });
+    question science5 = new question("What allows plants to make their own food?",
+        new string[] { "Chlorophyll", "Mitochondria", "Ribosomes", "Byzantium" });
+    question science6 = new question("What is the most common element in the universe?",
+        new string[] { "Hydrogen", "Oxygen", "Carbon", "Matter" });
+    question science7 = new question("What molecule allows us to breathe on Earth?",
+        new string[] { "Oxygen", "Hydrogen", "Helium", "Nitrogen" });
+    question science8 = new question("What are molecules made of?",
+        new string[] { "Atoms", "Cubes", "Cones", "Functions" });
+    question science9 = new question("What is the closest planet to the Sun?",
+        new string[] { "Mercury", "Earth", "Venus", "Pluto" });
+}
