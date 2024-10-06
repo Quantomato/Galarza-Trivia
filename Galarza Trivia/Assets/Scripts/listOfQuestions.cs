9a4ea30cddd2cb9524a5ca579836abb4d61d868c
FilmDragons <nagsid.puli@gmail.com>
2024-10-05 21:25:12 -0400

    Added Game Manager, schizo

--- Galarza Trivia/Assets/Scripts/listOfQuestions.cs
+++ Galarza Trivia/Assets/Scripts/listOfQuestions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -6,12 +7,14 @@ using UnityEngine;
 
 public class listOfQuestions
 {
-    question science0 = new question("What unit is electric power measured in?", 
-        new string[] {"Watts", "Ohms", "Newtons", "Fractals"});
-    question science1 = new question("What is the name of the scientist who discovered gravity?", 
+    question[] questionList = new question[10];
+
+    question science0 = new question("What unit is electric power measured in?",
+        new string[] { "Watts", "Ohms", "Newtons", "Fractals" });
+    question science1 = new question("What is the name of the scientist who discovered gravity?",
         new string[] { "Isaac Newton", "Robert Oppenheimer", "Albert Einstein", "Benjamin Franklin" });
-    question science2 = new question("What is the name of the galaxy we live in?", 
-        new string[] {"The Milky Way", "The Pinwheel", "The Andromeda", "The Triangulum"});
+    question science2 = new question("What is the name of the galaxy we live in?",
+        new string[] { "The Milky Way", "The Pinwheel", "The Andromeda", "The Triangulum" });
     question science3 = new question("What medicine is commonly used to treat bacteria?",
         new string[] { "Antibiotics", "Vaccines", "Antiseptics", "Neosporin" });
     question science4 = new question("What are the base instructions for all life?",
@@ -26,4 +29,30 @@ public class listOfQuestions
         new string[] { "Atoms", "Cubes", "Cones", "Functions" });
     question science9 = new question("What is the closest planet to the Sun?",
         new string[] { "Mercury", "Earth", "Venus", "Pluto" });
+    public question findScience(int num)
+    {
+        if (num == 0) { return science0; }
+        else if (num == 1) { return science1; }
+        else if (num == 2) { return science2; }
+        else if (num == 3) { return science3; }
+        else if (num == 4) { return science4; }
+        else if (num == 5) { return science5; }
+        else if (num == 6) { return science6; }
+        else if (num == 7) { return science7; }
+        else if (num == 8) { return science8; }
+        else if (num == 9) { return science9; }
+        else
+        {
+            Console.WriteLine("HELP ME IN LIST OF QUESTIONS, SCIENCE");
+            return science0;
+        }
+    }
+
+    public listOfQuestions()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            questionList[i] = findScience(i);
+        }
+    }
 }
